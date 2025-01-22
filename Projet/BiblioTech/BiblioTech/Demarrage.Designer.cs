1e82d640e55bd43cdd6c8e39fdd711df25f5da85
j0rg3-2005 <jorge.miguel.pinto.costa@gmail.com>
2025-01-22 10:59:33 +0100

    Ajout d'un fichier C# contenant un label de bienvenue et un bouton continuer

---
+++ Projet/BiblioTech/BiblioTech/Demarrage.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace BiblioTech
+{
+    partial class Demarrage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblBienvenue = new Label();
+            btnSuivant = new Button();
+            SuspendLayout();
+            // 
+            // lblBienvenue
+            // 
+            lblBienvenue.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lblBienvenue.AutoSize = true;
+            lblBienvenue.Font = new Font("SimSun", 72F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblBienvenue.Location = new Point(379, 133);
+            lblBienvenue.Name = "lblBienvenue";
+            lblBienvenue.Size = new Size(1267, 97);
+            lblBienvenue.TabIndex = 0;
+            lblBienvenue.Text = "Bienvenue dans BiblioTech";
+            lblBienvenue.Click += lblBienvenue_Click;
+            // 
+            // btnSuivant
+            // 
+            btnSuivant.AutoSize = true;
+            btnSuivant.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnSuivant.Font = new Font("Bodoni MT", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSuivant.Location = new Point(941, 467);
+            btnSuivant.Name = "btnSuivant";
+            btnSuivant.Size = new Size(152, 54);
+            btnSuivant.TabIndex = 1;
+            btnSuivant.Text = "Suivant";
+            btnSuivant.UseVisualStyleBackColor = true;
+            btnSuivant.Click += btnSuivant_Click;
+            // 
+            // Demarrage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Tan;
+            ClientSize = new Size(1463, 687);
+            Controls.Add(btnSuivant);
+            Controls.Add(lblBienvenue);
+            Name = "Demarrage";
+            Text = "frmDemarrage";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblBienvenue;
+        private Button btnSuivant;
+    }
+}
---
+++ Projet/BiblioTech/BiblioTech/Demarrage.cs
@@ -0,0 +1,32 @@
+namespace BiblioTech
+{
+    public partial class Demarrage : Form
+    {
+        public Demarrage()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            // Centrer horizontalement le texte et le bouton
+            lblBienvenue.Location = new Point((this.ClientSize.Width - lblBienvenue.Width) / 2, lblBienvenue.Location.Y);
+            btnSuivant.Location = new Point((this.ClientSize.Width - btnSuivant.Width) / 2, btnSuivant.Location.Y);
+        }
+
+        private void lblBienvenue_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSuivant_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
